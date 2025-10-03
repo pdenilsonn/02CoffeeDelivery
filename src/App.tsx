8aa913fffa40c6e0912aed210cc910fc343d7f38
pdenilsonn <denilso147@hotmail.com>
2025-10-03 11:22:15 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './global.css'
+
+export function App() {
+  return (
+    <div className="flex justify-center align-middle">
+      <h1 className="text-6xl">Hello World! </h1>
+    </div>
+  )
+}
